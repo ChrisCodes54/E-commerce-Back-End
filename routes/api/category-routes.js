429dea8c0586eeac45ab7532085046890d7d1346
ChrisCodes54 <christangarife95@gmail.com>
2021-12-13 17:26:13 -0500

    added routes to the product Js, tested in insomnia currently wokring as of this push

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,8 +4,6 @@ const { Category, Product } = require("../../models");
 // The `/api/categories` endpoint
 
 router.get("/", (req, res) => {
-    // find all categories
-    // be sure to include its associated Products
     Category.findAll({
         include: [Product],
     })
@@ -14,8 +12,7 @@ router.get("/", (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
-    // find one category by its `id` value
-    // be sure to include its associated Products
+   
     try {
         const categoryData = await Category.findOne({
             where: {
@@ -31,7 +28,6 @@ router.get("/:id", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-    // create a new category
     try {
         const categoryData = await Category.create(req.body);
         res.status(200).json(categoryData);
@@ -41,7 +37,6 @@ router.post("/", async (req, res) => {
 });
 
 router.put("/:id", async (req, res) => {
-    // update a category by its `id` value
     try {
         const categoryData = await Category.update(
             {
@@ -60,7 +55,6 @@ router.put("/:id", async (req, res) => {
 });
 
 router.delete("/:id", async (req, res) => {
-    // delete a category by its `id` value
     try {
       const categoryData = await Category.destroy({
         where: {
